20996fef19d514396feef815f78de085fb732a56
RenSunB0y <150889605+RenSunB0y@users.noreply.github.com>
2024-11-05 23:04:48 +0100

    OSKOUUUUUUR

---
+++ ConsoleApp1/De.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuatreVingtEtUn
+{
+    internal class De
+    {"
+        public int NbFaces;
+        private Random random = new Random();
+
+        public De(int nbFaces = 6)
+        {
+            this.NbFaces = nbFaces;
+        }
+
+        public int Face { get; private set; }
+
+        public override string ToString()
+        {
+            string toString = String.Format("Vous avez lancé un dé à {0} faces et vous avez obtenu un {1}", this.NbFaces, this.Face);
+            return toString;
+        }
+
+        public int Lancer()
+        {
+            this.Face = random.Next(1, NbFaces + 1);
+            return this.Face;
+        }
+    }
+}
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5573b9c7948f807ea2b36e7a6aca110e5ad79134")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc89688d5f9d7ac95f2d62efcdd9bac0e96af2ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
